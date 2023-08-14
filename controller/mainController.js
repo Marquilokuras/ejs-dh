759da64b973bd9cf6df3c3ab75394fcb0ca99bd8
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-08-14 11:32:40 -0300

    Carpeta database con los platos

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,36 +1,5 @@
 const path = require('path');
-
-const arrayPlatos = [
-
-    {
-        id:"1",
-        nombre: "Carpaccio fresco",
-        descripcion: "Entrada Carpaccio de salmón con cítricos",
-        precio: 65.50,
-        imagen: "/imagenes/Carpaccio-de-salmon.jpg"
-    },
-    {
-        id:"2",
-        nombre: "Risotto de berenjena",
-        descripcion: "Risotto de berenjena y queso de cabra",
-        precio: 47.00,
-        imagen: "/imagenes/Risotto-berenjena-queso-cabra.jpg"
-    },
-    {
-        id:"3",
-        nombre: "Mousse de arroz",
-        descripcion: "Mousse de arroz con leche y aroma de azahar",
-        precio: 27.50,
-        imagen: "/imagenes/Mousse-de-arroz-con-leche.jpg"
-    },
-    {
-        id:"4",
-        nombre: "Espárragos blancos",
-        descripcion: " Espárragos blancos con vinagreta de verduras y jamón ibérico",
-        precio: 37.50,
-        imagen: "/imagenes/esparragos.png"
-    },
-]
+const arrayPlatos = require('../database/platos')
 
 const mainController = {
     renderHome: (req, res) => {
