1b4eb547d778ec06dc27f095fb675f10e240552a
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-08-14 23:08:09 -0300

    Arreglitos

--- database/platos.js
+++ database/platos.js
@@ -4,28 +4,28 @@ const platos = [
         nombre: "Carpaccio fresco",
         descripcion: "Entrada Carpaccio de salmón con cítricos",
         precio: 65.50,
-        imagen: "/images/Carpaccio-de-salmon.jpg"
+        imagen: "Carpaccio-de-salmon.jpg"
     },
     {
         id:"2",
         nombre: "Risotto de berenjena",
         descripcion: "Risotto de berenjena y queso de cabra",
         precio: 47.00,
-        imagen: "/images/Risotto-berenjena-queso-cabra.jpg"
+        imagen: "Risotto-berenjena-queso-cabra.jpg"
     },
     {
         id:"3",
         nombre: "Mousse de arroz",
         descripcion: "Mousse de arroz con leche y aroma de azahar",
         precio: 27.50,
-        imagen: "/images/Mousse-de-arroz-con-leche.jpg"
+        imagen: "Mousse-de-arroz-con-leche.jpg"
     },
     {
         id:"4",
         nombre: "Espárragos blancos",
         descripcion: " Espárragos blancos con vinagreta de verduras y jamón ibérico",
         precio: 37.50,
-        imagen: "/images/esparragos.png"
+        imagen: "esparragos.png"
     },
 ]
 
